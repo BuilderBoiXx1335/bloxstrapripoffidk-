ba7d1867b5b0f1bae527046585638308a193982b
BuilderBoiXx1335 <Workatapizzaplace11@gmail.com>
2025-09-20 22:05:53 -0400

    Update ModLoader.cs

--- Mods/ModLoader.cs
+++ Mods/ModLoader.cs
@@ -1,1 +1,17 @@
+using System.IO;
+
+public static class ModLoader
+{
+    public static void LoadMods(string modsDirectory)
+    {
+        if (!Directory.Exists(modsDirectory)) return;
+
+        string[] modFiles = Directory.GetFiles(modsDirectory);
+        foreach (string mod in modFiles)
+        {
+            Console.WriteLine($"Found mod: {Path.GetFileName(mod)}");
+            // You could add DLL injection or file copying here
+        }
+    }
+}
 
