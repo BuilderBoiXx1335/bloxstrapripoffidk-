020ca67620033d4cbf337ef410cd2593166bda2f
BuilderBoiXx1335 <Workatapizzaplace11@gmail.com>
2025-09-20 22:06:08 -0400

    Update RichPresence.cs

--- Discord/RichPresence.cs
+++ Discord/RichPresence.cs
@@ -1,1 +1,29 @@
+using DiscordRPC;
+
+public static class RichPresence
+{
+    private static DiscordRpcClient client;
+
+    public static void Init()
+    {
+        client = new DiscordRpcClient("YOUR_DISCORD_APP_ID");
+        client.Initialize();
+
+        client.SetPresence(new RichPresence()
+        {
+            Details = "Playing Roblox",
+            State = "Using Custom Launcher",
+            Assets = new Assets()
+            {
+                LargeImageKey = "roblox",
+                LargeImageText = "Roblox"
+            }
+        });
+    }
+
+    public static void Shutdown()
+    {
+        client.Dispose();
+    }
+}
 
